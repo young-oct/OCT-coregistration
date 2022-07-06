1b654eb3a27bf6269b8b576bcd0995f857740000
young-oct <>
2022-07-06 11:43:28 -0300

    added new dataset and updated parameters

--- scripts/oct2dicom(corrected).py
+++ scripts/oct2dicom(corrected).py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8fca822578bdae7a170663a52d808ddc2181b0da9ab7d7f11828e42edc4b0eb
+oid sha256:84d78a1269b48e9324fd7432b85e80007a262f8e32c953261d00d86b84bbccb1
 size 2037
